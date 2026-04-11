33f00d4f99397c4ca9e82333a62a45e770de00fc
Wesley2117 <wesleysant741@gmail.com>
2026-04-11 09:57:59 -0300

    feat: Criando melhorias no back e usando para conectar ao front

--- src/modules/users/user.service.ts
+++ src/modules/users/user.service.ts
@@ -4,10 +4,40 @@ import bcrypt from 'bcrypt';
 
 export class UserService {
     async getProfile(userId: string) {
-        return await prisma.user.findUnique({
-            where: { id: userId},
-            select: { id: true, name: true, email: true, createdAt: true }
-        });
+        const [user, salesCount, productsCount, uniqueClient] = await Promise.all([
+            prisma.user.findUnique({
+                where: {id: userId},
+                select: {id: true, name: true, email: true, createdAt: true}
+            }),
+
+            prisma.sale.count({
+                where: {userId}
+            }), 
+
+            prisma.product.count({
+                where: { userId, active: true}
+            }), 
+
+            prisma.sale.findMany({
+                where: {
+                    userId, 
+                    customerPhone: { notIn: null, not: "" } 
+                }, 
+                distinct: ['customerPhone'],
+                select: { customerPhone: true }
+            })
+        ]);
+
+        if (!user) throw new Error("Usuário não encontrado");
+
+        return {
+            ...user,
+            stats: {
+                sales: salesCount, 
+                products: productsCount, 
+                clients: uniqueClient.length
+            }
+        };
     }
 
     async updateProfile(userId: string, data: { name?: string; email?: string }) {
